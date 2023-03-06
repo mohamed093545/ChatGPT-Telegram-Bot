27b18ab5127cead62c4c04d8cfb81191a194c17d
mohamed093545 <119966552+mohamed093545@users.noreply.github.com>
2023-03-06 14:03:07 +0200

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -66,8 +66,8 @@ def start(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'Bitly'
+        username = 'Bitly09715_Bot'
         update.message.reply_text('Hi')
         return 
     else:
@@ -93,16 +93,16 @@ def reset(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'Bitly'
+        username = 'Bitly09715_Bot'
         update.message.reply_text('Bot has been reset, send a message!')
         return
     if tim == 1:
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'Bitly'
+        username = 'Bitly09715_Bot'
         update.message.reply_text('Bot has been reset, send a message!')
         return 
     else:
@@ -128,8 +128,8 @@ def retry(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'Bitly'
+        username = 'Bitly09715_Bot'
         update.message.reply_text('Send a message!')
         return 
     else:
@@ -189,8 +189,8 @@ def wait(bot, update, botname, username, new):
                 cache = None
                 qcache = None
                 user = ""
-                username = 'harshitethic_bot'
-                botname = 'Harshit Ethic'
+                username = 'Bitly09715_Bot'
+                botname = 'Bitly'
                 update.message.reply_text('Timer has run down, bot has been reset to defaults.')
                 running = False
     else:
